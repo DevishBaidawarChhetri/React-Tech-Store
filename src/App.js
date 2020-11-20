b915157b6e62aa8e1d273f9751adbc029b3f372a
Devish Baidawar Chhetri <devishbaidawar@gmail.com>
2020-11-20 11:40:17 +0545

    Project Initiated :fire:

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './css/App.css';
+
+function App () {
+  return (
+    <h1>Hello from App.js</h1>
+  );
+}
+
+export default App;
