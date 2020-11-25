28f663ec50c930642fadbd07700f809bbbb76843
Devish Baidawar Chhetri <devishbaidawar@gmail.com>
2020-11-25 10:51:51 +0545

    Rendered Contacts :telephone_receiver:

---
+++ src/components/Contact/contact.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import Title from '../PageInfo/Title'
+export default function contact () {
+  return (
+    <>
+      <section className="py-5">
+        <div className="row m-0">
+          <div className="col-10 mx-auto col-md-6 my-3">
+            <Title title="contacts" center />
+            <form className="mt-5">
+
+              {/* Full Name */ }
+              <div className="form-group">
+                <input
+                  type="text"
+                  name="fullName"
+                  className="form-control"
+                  placeholder="John Doe"
+                  required
+                />
+              </div>
+              {/* Email */ }
+              <div className="form-group">
+                <input
+                  type="email"
+                  name="email"
+                  className="form-control"
+                  placeholder="johndoe@techstore.com"
+                  required
+                />
+              </div>
+
+              {/* Subject */ }
+              <div className="form-group">
+                <input
+                  type="text"
+                  name="subject"
+                  className="form-control"
+                  placeholder="Subject Here"
+                  required
+                />
+              </div>
+              {/* Message */ }
+              <div className="form-group">
+                <textarea
+                  className="form-control"
+                  name="message"
+                  rows="5"
+                  placeholder="Hello World!"
+                  required
+                ></textarea>
+              </div>
+              <div className="form-group mt-3">
+                <input type="submit" className="form-control main-link" />
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import Hero from '../components/Banner/Hero';
+import ContactBackground from '../images/contactBcg.jpeg'
+import Title from '../components/PageInfo/Title';
+import Contact from '../components/Contact/contact';
 
 export default function ContactPage () {
   return (
     <>
-      hello from ContactPage.js
+      <Hero
+        title="contact us"
+        img={ ContactBackground }
+      >
+        <h6>Contact us for any queries.</h6>
+      </Hero>
+      <Contact />
     </>
   )
 }
